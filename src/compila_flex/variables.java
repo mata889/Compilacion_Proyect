ecd4385ff14bb526477f03d7b2076cec3c941b63
unknown <rasanchez@unitec.edu>
2020-11-03 16:43:54 -0600

    Recorrido a medias

--- src/compila_flex/variables.java
+++ src/compila_flex/variables.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package compila_flex;
 
 /**
@@ -10,5 +5,57 @@ package compila_flex;
  * @author Ricardo
  */
 public class variables {
+    public String tipo;
+    public String id;
+    public String ambito;
+    public String offset;
+
+
+    public variables(String tipo, String id, String ambito) {
+        this.tipo = tipo;
+        this.id = id;
+        this.ambito = ambito;
+    }
+    
+    //IGNOREN ESTE POR AHORA, ES PARA CUANDO USEMOS ASSEMBLER
+    public variables(String tipo, String id, String ambito, String offset) {
+        this.tipo = tipo;
+        this.id = id;
+        this.ambito = ambito;
+        this.offset = offset;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAmbito() {
+        return ambito;
+    }
+
+    public void setAmbito(String ambito) {
+        this.ambito = ambito;
+    }
+
+    public String getOffset() {
+        return offset;
+    }
+
+    public void setOffset(String offset) {
+        this.offset = offset;
+    }
+    
     
 }
