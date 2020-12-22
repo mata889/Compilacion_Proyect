aa1e2c4a79511926c88663a40bd2841bc3852ca1
DiegoMatamoros <dam_900@unitec.edu>
2020-12-22 05:25:25 -0600

    ejemplos correctos

--- MIPS.asm
+++ MIPS.asm
@@ -1,30 +1,39 @@
 .data
+_numGlobal:      .word 0
+_msg0:     .asciiz "awa"
    .text
    .globl main
-_awa:
+_imprimir:
        sw $fp, -4($sp)
        sw $ra, -8($sp)
-       sw $s0, -12($sp)
-       sw $s1, -16($sp)
-       lw $v0, _par1
-       b _fin_awa
-_fin_awa:
+       lw $v0, _ret
+       b _fin_imprimir
+_fin_imprimir:
        move $sp, $fp
        lw $fp, -4($sp)
        lw $ra, -8($sp)
-       lw $s0, -16($sp)
-       lw $s1, -20($sp)
        jr $ra
 main:
        move $fp, $sp
-       lw $a0, _parametro1
-       lw $a1, _parametro2
-       lw $a2, _parametro3
-       lw $a3, _parametro4
-       lw $t0, _parametro5
-       sw $t0, -4($sp)
-       lw $t0, _parametro6
-       sw $t0, -8($sp)
-       jal _awa
+_etiq2:
+_etiq3:
+
+       li $v0,       b _etiq2
+_etiq5:
+
+       li $v0,
+       li $v0, 4
+       la $a0, _msg0
+       syscall_etiq7:
+       lw $t0, _i
+       li $t1, 6
+       blt $t0, $t1, _etiq8
+       b _null
+_etiq9:
+       b _etiq7
+_etiq8:
+_etiq11:
+
+       li $v0,       b _etiq9
        li $v0,10
        syscall
